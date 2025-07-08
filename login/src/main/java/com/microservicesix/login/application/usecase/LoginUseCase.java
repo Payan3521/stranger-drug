73fc0d7bf73f6e757a96bb023d528b627c8fe04c
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-07-08 13:43:29 -0500

    Agregando cpaz de dominio CON ERRORES

---
+++ login/src/main/java/com/microservicesix/login/application/usecase/LoginUseCase.java
@@ -0,0 +1,17 @@
+package com.microservicesix.login.application.usecase;
+
+import com.microservicesix.login.domain.model.LoginRequest;
+import com.microservicesix.login.domain.model.LoginResponse;
+import com.microservicesix.login.domain.model.User;
+import com.microservicesix.login.domain.port.in.ILogin;
+
+public class LoginUseCase implements ILogin{
+
+    @Override
+    public LoginResponse login(LoginRequest loginRequest) {
+        User user = 
+    }
+    
+     
+    
+}
---
+++ login/src/main/java/com/microservicesix/login/domain/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.microservicesix.login.domain.exception;
+
+public class InvalidCredentialsException extends RuntimeException{
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
+
+    public InvalidCredentialsException(){
+        super("Invalid credentials");
+    }
+}
