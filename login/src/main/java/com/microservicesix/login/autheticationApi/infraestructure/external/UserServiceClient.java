0ef5e9ccce51aae28f905a5a4c65df32c4479c14
payan3521 <pablobedoya3521@gmail.com>
2025-07-10 11:55:24 -0500

    listo para ser finalizado

--- login/src/main/java/com/microservicesix/login/autheticationApi/infraestructure/external/UserServiceClient.java
+++ login/src/main/java/com/microservicesix/login/autheticationApi/infraestructure/external/UserServiceClient.java
@@ -3,10 +3,8 @@ package com.microservicesix.login.autheticationApi.infraestructure.external;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.microservicesix.login.autheticationApi.infraestructure.external.dto.ApiResponse;
 import com.microservicesix.login.autheticationApi.infraestructure.external.dto.UserResponse;
-
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "users-service", path = "/register")
