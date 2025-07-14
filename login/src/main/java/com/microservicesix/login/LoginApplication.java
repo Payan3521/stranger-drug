4d7d01288435b4818b309dc53a0c43f38c031b9b
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-07-14 12:12:21 -0500

    Agregando .env en login, con sus variables de entorno

--- login/src/main/java/com/microservicesix/login/LoginApplication.java
+++ login/src/main/java/com/microservicesix/login/LoginApplication.java
@@ -5,13 +5,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class LoginApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMissing()
+			.load();
+
+			if(dotenv!=null){
+				dotenv.entries().forEach(entry -> {
+					if(System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey())==null){
+						System.setProperty(entry.getKey(), entry.getValue());
+					}
+				});
+			}
 		SpringApplication.run(LoginApplication.class, args);
 	}
-
 }
